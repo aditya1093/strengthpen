92ae2d411e0a64ac888ddc04513e552cde5cca36
tapiwan <dirk.englert@web.de>
2016-04-03 23:09:07 +0200

    Added view for adding a new plan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('title')
-    Dashboard
+    My Plans
 @endsection
 
 @section('content')
@@ -9,10 +9,33 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Add a new plan</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <form action="{{ url('plan') }}" method="POST" class="form-horizontal">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label for="plan-name" class="col-sm-3 control-label">Name</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="plan-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="plan-description" class="col-sm-3 control-label">Description</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="plan-description" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Plan
+                                </button>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
