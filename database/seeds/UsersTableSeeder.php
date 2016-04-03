31bb9cf93b078037df4d76abb3084ba81ef3321f
tapiwan <dirk.englert@web.de>
2016-04-03 21:42:55 +0200

    Added seed for users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@strengthpen.de',
+            'password' => '$2y$10$5CsSaBjXoIL1Aabv5d.5B.bIEizkZVvUtjFfffiXn8R087QbV0HSi' // = strengthpen
+        ]);
+    }
+}
