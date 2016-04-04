8b5aa4273d09dafa2f556f51828707b69d90f3c0
tapiwan <dirk.englert@web.de>
2016-04-04 23:31:04 +0200

    Plan now added & edited via modal

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -8,43 +8,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @include('errors.common')
+
+            <!-- Title and Toolbar -->
             <div class="panel panel-default">
-                <div class="panel-heading">Add a new plan</div>
-
-                <div class="panel-body">
-                    @include('errors.common')
-
-                    <form action="{{ url('/plan') }}" method="POST" class="form-horizontal">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group">
-                            <label for="plan-name" class="col-sm-3 control-label">Name</label>
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="plan-name" class="form-control">
-                            </div>
+                <div class="panel-heading">
+                    <h3 class="panel-title pull-left"><strong>My Plans</strong></h3>
+                    <div class="panel-toolbar pull-right">
+                        <div class="btn-group" role="group" aria-label="...">
+                            <button class="btn btn-sm btn-primary" data-toggle="modal" data-target=".bs-add-plan-modal">
+                                <i class="fa fa-btn fa-plus"></i> Add Plan
+                            </button>
                         </div>
-
-                        <div class="form-group">
-                            <label for="plan-description" class="col-sm-3 control-label">Description</label>
-                            <div class="col-sm-6">
-                                <input type="text" name="description" id="plan-description" class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="fa fa-btn fa-plus"></i> Add Plan
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">My Plans</div>
-
+                <!-- Show all plans -->
                 <div class="panel-body">
                     @if(count($plans) !== 0)
                         @foreach ($plans as $plan)
@@ -56,16 +35,16 @@
                                 {{ $plan->description }}
                             </div>
                             <div class="panel-footer">
-                                <form class="inline-btn-form" action="{{ url('/plan/'.$plan->id) }}" method="POST">
+                                <form class="inline-block" action="{{ url('/plan/'.$plan->id) }}" method="POST">
                                     {!! csrf_field() !!}
                                     {!! method_field('DELETE') !!}
 
-                                    <button type="submit" class="btn btn-danger">
+                                    <button type="submit" class="btn btn-sm btn-danger">
                                         <i class="fa fa-btn fa-trash"></i> Delete
                                     </button>
                                 </form>
 
-                                <button class="btn btn-primary" data-toggle="modal" data-target=".bs-edit-plan-modal"
+                                <button class="btn btn-sm btn-primary" data-toggle="modal" data-target=".bs-edit-plan-modal"
                                         data-form-action="{{ url('/plan/'.$plan->id) }}"
                                         data-plan-name="{{ $plan->name }}"
                                         data-plan-description="{{ $plan->description }}">
@@ -80,44 +59,9 @@
                 </div>
             </div>
 
-            <div class="modal fade bs-edit-plan-modal" tabindex="-1" role="dialog">
-                <form id="updateForm" action="" method="POST">
-                    {!! csrf_field() !!}
-                    {!! method_field('PUT') !!}
-
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title">Edit Plan</h4>
-                            </div>
-
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="new-plan-name" class="col-sm-3 control-label">Name</label>
-                                    <div>
-                                        <input type="text" name="name" id="new-plan-name" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="new-plan-description" class="col-sm-3 control-label">Description</label>
-                                    <div>
-                                        <input type="text" name="description" id="new-plan-description" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-btn fa-close"></i> Close</button>
-                                <button type="submit" class="btn btn-primary"><i class="fa fa-btn fa-floppy-o"></i> Save changes</button>
-                            </div>
-                        </div><!-- /.modal-content -->
-                    </div><!-- /.modal-dialog -->
-                </form>
-            </div><!-- /.modal -->
-
+            <!-- Modals -->
+            @include('plans.modals.add-plan')
+            @include('plans.modals.edit-plan')
         </div>
     </div>
 </div>
---
+++ resources/views/plans/modals/edit-plan.blade.php
@@ -0,0 +1,37 @@
+<div class="modal fade bs-edit-plan-modal" tabindex="-1" role="dialog">
+    <form id="updateForm" action="" method="POST">
+        {!! csrf_field() !!}
+        {!! method_field('PUT') !!}
+
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Edit Plan</h4>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="new-plan-name" class="col-sm-3 control-label">Name</label>
+                        <div>
+                            <input type="text" name="name" id="new-plan-name" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="new-plan-description" class="col-sm-3 control-label">Description</label>
+                        <div>
+                            <input type="text" name="description" id="new-plan-description" class="form-control">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-btn fa-close"></i> Cancel</button>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-btn fa-floppy-o"></i> Save changes</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </form>
+</div><!-- /.modal -->
