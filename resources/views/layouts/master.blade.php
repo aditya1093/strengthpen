7916674d5ced69514c9117419b423208a26838a9
tapiwan <dirk.englert@web.de>
2016-04-05 00:25:06 +0200

    Changed relative paths to absolute paths

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,14 +8,14 @@
     <title>Strengthpen - @yield('title')</title>
 
     <!-- Stylesheets -->
-    <link href="styles/strengthpen.css" rel='stylesheet' type='text/css'>
+    <link href="/styles/strengthpen.css" rel='stylesheet' type='text/css'>
 
     <!-- Fonts -->
-    <link href="libs/font-awesome-4.5.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="/libs/font-awesome-4.5.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="libs/bootstrap-3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="/libs/bootstrap-3.3.6/css/bootstrap-theme.min.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 </head>
 
@@ -25,9 +25,9 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="libs/jquery-2.2.2/jquery.min.js"></script>
-    <script src="libs/bootstrap-3.3.6/js/bootstrap.min.js"></script>
+    <script src="/libs/jquery-2.2.2/jquery.min.js"></script>
+    <script src="/libs/bootstrap-3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-    <script src="scripts/strengthpen.js"></script>
+    <script src="/scripts/strengthpen.js"></script>
 </body>
 </html>
