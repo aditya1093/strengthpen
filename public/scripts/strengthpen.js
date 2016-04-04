1361124aa305d22d5d0aa3edbadb1d98306585a4
tapiwan <dirk.englert@web.de>
2016-04-04 22:52:25 +0200

    Fill edit plan modal with selected plan data

--- public/scripts/strengthpen.js
+++ public/scripts/strengthpen.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
     (function() {
-        //Do Stuff
+        open_edit_plan_modal();
     })();
+
+    function open_edit_plan_modal() {
+        $('.bs-edit-plan-modal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget);
+            var btnData = button.data();
+
+            var formAction      = btnData.formAction;
+            var planName        = btnData.planName;
+            var planDescription = btnData.planDescription;
+
+            var modal  = $(this);
+            modal.find('#updateForm').attr( "action", formAction);
+            modal.find('.modal-title').html('Edit Plan <strong>' + planName + '</strong>');
+            modal.find('#new-plan-name').val(planName);
+            modal.find('#new-plan-description').val(planDescription);
+        });
+    }
 });
