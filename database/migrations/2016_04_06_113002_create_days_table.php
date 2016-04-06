3f3275a4ea55dbe6adc1291a2ef8c77105abbc32
Dirk Englert <dirk.englert@web.de>
2016-04-06 13:44:00 +0200

    Created migration

---
+++ database/migrations/2016_04_06_113002_create_days_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('plan_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('days');
+    }
+}
