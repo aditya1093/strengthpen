cd57382d8c754c2b7c3d4f2a0f5d5623f877c5c4
tapiwan <dirk.englert@web.de>
2016-04-07 17:31:04 +0200

    Added seeder for days table

--- database/seeds/DaysTableSeeder.php
+++ database/seeds/DaysTableSeeder.php
@@ -12,6 +12,24 @@ class DaysTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 1; $i <= 25; $i++) {
+            Day::create([
+                'plan_id' => '1',
+                'title' => 'GST Upper Body',
+                'date' => '2016-04-07',
+                'schedule' => 'Front Lever 3x8s tuck position
+Planche 3x12s green band assistance
+Weighted Pullups 5x6r +10kg/+10g/+20kg/+30kg/+40kg
+Weighted Pushups 5x8r +20kg/+20kg/+20kg/+20kg/+20kg'
+            ]);
+
+            Day::create([
+                'plan_id' => '1',
+                'title' => 'OMOM Back Squat',
+                'date' => '2016-04-08',
+                'schedule' => 'Squat 4x4r 75kg/90kg/110kg/130kg
+Squat 4 reps every minute on the minute for 10 minutes'
+            ]);
+        }
     }
 }
